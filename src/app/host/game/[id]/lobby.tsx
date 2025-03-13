ac73135bcf5aa3a6db1208e6fef24e6a116ebe74
Aaron Will Djaba <papiliocurtis@gmail.com>
2025-03-13 07:54:55 +0000

    updated the lobby url

--- src/app/host/game/[id]/lobby.tsx
+++ src/app/host/game/[id]/lobby.tsx
@@ -1,60 +1,60 @@
-import { Participant, supabase } from '@/types/types'
-import { useQRCode } from 'next-qrcode'
+import {Participant, supabase} from '@/types/types'
+import {useQRCode} from 'next-qrcode'
 
 export default function Lobby({
-  participants: participants,
-  gameId,
-}: {
-  participants: Participant[]
-  gameId: string
+                                  participants: participants,
+                                  gameId,
+                              }: {
+    participants: Participant[]
+    gameId: string
 }) {
-  const { Canvas } = useQRCode()
+    const {Canvas} = useQRCode()
 
-  const onClickStartGame = async () => {
-    const { data, error } = await supabase
-      .from('games')
-      .update({ phase: 'quiz' })
-      .eq('id', gameId)
-    if (error) {
-      return alert(error.message)
+    const onClickStartGame = async () => {
+        const {data, error} = await supabase
+            .from('games')
+            .update({phase: 'quiz'})
+            .eq('id', gameId)
+        if (error) {
+            return alert(error.message)
+        }
     }
-  }
 
-  return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="flex justify-between m-auto bg-black p-12">
-        <div className="w-96">
-          <div className="flex justify-start flex-wrap pb-4">
-            {participants.map((participant) => (
-              <div
-                className="text-xl m-2 p-2 bg-pink-500"
-                key={participant?.id}
-              >
-                {participant?.nickname}
-              </div>
-            ))}
-          </div>
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <div className="flex justify-between m-auto bg-black p-12">
+                <div className="w-96">
+                    <div className="flex justify-start flex-wrap pb-4">
+                        {participants.map((participant) => (
+                            <div
+                                className="text-xl m-2 p-2 bg-pink-500"
+                                key={participant?.id}
+                            >
+                                {participant?.nickname}
+                            </div>
+                        ))}
+                    </div>
 
-          <button
-            className="mx-auto bg-white py-4 px-12 block text-black"
-            onClick={onClickStartGame}
-          >
-            Start Game
-          </button>
+                    <button
+                        className="mx-auto bg-white py-4 px-12 block text-black"
+                        onClick={onClickStartGame}
+                    >
+                        Start Game
+                    </button>
+                </div>
+                <div className="pl-4">
+                    {/* <img src="/qr.png" alt="QR code" /> */}
+                    <Canvas
+                        text={`https://quizzer-ten-pi.vercel.app//game/${gameId}`}
+                        options={{
+                            errorCorrectionLevel: 'M',
+                            margin: 3,
+                            scale: 4,
+                            width: 400,
+                        }}
+                    />
+                </div>
+            </div>
         </div>
-        <div className="pl-4">
-          {/* <img src="/qr.png" alt="QR code" /> */}
-          <Canvas
-            text={`https://kahoot-alternative.vercel.app/game/${gameId}`}
-            options={{
-              errorCorrectionLevel: 'M',
-              margin: 3,
-              scale: 4,
-              width: 400,
-            }}
-          />
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
